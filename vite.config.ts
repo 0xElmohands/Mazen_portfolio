e61ed97f3e6b848b0c8495f7b2da00647fa3136e
0xElmohands <mazenayman.b0ss@gmail.com>
2026-05-10 22:49:34 +0300

    fix: set base path to '/' for Vercel + add vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Mazen_portfolio/',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
